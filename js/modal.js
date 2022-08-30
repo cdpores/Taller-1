f0410d998dd2801defc6ec78d9c74066cd889877
cdpores <verde123456jm@gmail.com>
2022-08-30 13:43:51 -0500

    Realizacion modal

---
+++ js/modal.js
@@ -0,0 +1,23 @@
+/*Acceder al botón para abrir el modal */
+
+let btnmodal=document.getElementById("abrirModal");
+
+//Acceder ventana modal
+let venmodal=document.getElementById("ventanaModal");
+
+//Acceder al botón Cerrar de la ventana modal
+let cermodal=document.querySelector(".cerrarModal");
+
+btnmodal.addEventListener("click",()=>{
+    venmodal.style.display="block";
+})
+
+cermodal.addEventListener("click",()=>{
+    venmodal.style.display="none"; 
+})
+
+window.addEventListener("click",(e)=>{
+if(e.target==venmodal){
+    venmodal.style.display="none"; 
+}
+})
